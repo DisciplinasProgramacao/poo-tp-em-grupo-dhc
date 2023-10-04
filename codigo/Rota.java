f85c9d6b8cec95ddb971c1833691bf9443b04c9c
sarahandrade <89746332+sarahandrade@users.noreply.github.com>
2023-10-03 21:59:27 -0300

    Update Rota.java

---
+++ codigo/Rota.java
@@ -0,0 +1,11 @@
+package codigo;
+
+public class Rota {
+    
+    private double quilometragem;
+    private Data data;
+
+    public String Relatorio (){
+        System.out.print("Data da Rota:" +data + "quilometragem:"+quilometragem);
+    }
+}
